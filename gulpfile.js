1b3c095c9a40f5e7c78eca208a0b9de772d61b58
Mihael Tomic <mihael.tomich@gmail.com>
2015-08-28 12:19:04 +0200

    Added new files, mixins are not working

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+
+//Plugins
 var mixins = require('postcss-mixins');
 var simplevars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
@@ -7,27 +9,33 @@ var customMedia = require('postcss-custom-media');
 var cssnano = require('cssnano');
 var pxtorem = require('postcss-pxtorem');
 var autoprefixer = require('autoprefixer-core');
+var atImport = require("postcss-import");
 
+//Processors
 gulp.task('css', function (){
   processors = [
+    mixins,
     pxtorem({
       prop_white_list: [],
       selector_black_list: ['a', 'body']
     }),
-    mixins,
+    atImport ({
+      from: "css/app.css"
+    }),
     simplevars,
     nested,
     customMedia,
-    cssnano,
+    // cssnano,
     autoprefixer ({ 
       browsers: ['last 4 version', 'IE 8'] 
     })
   ];
-  return gulp.src('css/style.css')
+  return gulp.src('css/app.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest('dist'))
-;});
+  ;});
 
+//Watch task
 gulp.task('watch', ['css'],
   function () {
     gulp.watch('css/*.css', ['css']);
